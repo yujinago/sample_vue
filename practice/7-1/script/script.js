4195caad7ec7b8e86d24f58d7066d370f5b392d4
yujinago <yuji.yuji.0813@gmail.com>
2023-11-10 12:10:06 +0900

    add_7-1_methods

--- practice/7-1/script/script.js
+++ practice/7-1/script/script.js
@@ -5,5 +5,26 @@ new Vue({
       todos: [],
       text: ''
     };
+  },
+  methods: {
+    inputText(e) {
+      this.text = e.target.value;
+    },
+    addTodo() {
+      if (!this.text) return;
+
+      const text = this.text;
+      const id = Math.ceil(Math.random() * 1000);
+      const todo = {
+        id,
+        text,
+        isDone: false
+      };
+      this.todos.push(todo);
+      this.resetText();
+    },
+    resetText() {
+      this.text = '';
+    }
   }
 });
