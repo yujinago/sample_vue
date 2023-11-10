6557f0304893662a5bf29b3257f87e76a69b021e
yujinago <yuji.yuji.0813@gmail.com>
2023-11-10 12:31:35 +0900

    create_7-2_script

---
+++ practice/7-2/script/script.js
@@ -0,0 +1,38 @@
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      todos: [],
+      text: ''
+    };
+  },
+  methods: {
+    inputText(e) {
+      this.text = e.target.value;
+    },
+    addTodo() {
+      if (!this.text) return;
+
+      const text = this.text;
+      const id = Math.ceil(Math.random() * 1000);
+      const todo = {
+        id,
+        text,
+        isDone: false
+      };
+      this.todos.push(todo);
+      this.resetText();
+    },
+    resetText() {
+      this.text = '';
+    }
+  },
+  computed: {
+    doneTodo() {
+      return this.todos.filter( todo => todo.isDone === true );
+    },
+    incompleteTodo() {
+      return this.todos.filter( todo => todo.isDone === false );
+    }
+  }
+});
