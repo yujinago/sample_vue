8388a174889dcce7cfbed96a89a270e5c12260cf
yujinago <yuji.yuji.0813@gmail.com>
2023-11-11 17:46:49 +0900

    add_8-1_component

--- practice/8-1/script/script.js
+++ practice/8-1/script/script.js
@@ -7,3 +7,33 @@ const imageThumbnail = Vue.component('image-thumbnail', {
   },
   template: `<div class="img-box" :style="{ backgroundImage: 'url(' + path + ')' }" @click="$emit('clickimage')"></div>`
 });
+
+const app = new Vue({
+  el: '#app',
+  component: {
+    'image-thumbnail': imageThumbnail
+  },
+  date() {
+    return {
+      images: [{
+        path: './img/img1.jpg',
+      },{
+        path: './img/img2.jpg',
+      },{
+        path: './img/img3.jpg',
+      },{
+        path: './img/img4.jpg',
+      },{
+        path: './img/img5.jpg',
+      },{
+        path: './img/img6.jpg',
+      },{
+        path: './img/img7.jpg',
+      },{
+        path: './img/img8.jpg',
+      },{
+        path: './img/img9.jpg'
+      }]
+    };
+  }
+});
